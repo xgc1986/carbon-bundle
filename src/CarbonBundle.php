6f0e36354a9ecae5b083a69551ff887c38901e31
Xavier Gonzalez <xavi@nektria.com>
2017-09-02 18:32:45 +0200

    remove unused dependencies

--- src/CarbonBundle.php
+++ src/CarbonBundle.php
@@ -3,10 +3,7 @@ declare(strict_types=1);
 
 namespace Xgc\CarbonBundle;
 
-use LogicException;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Xgc\CarbonBundle\DependencyInjection\CarbonExtension;
 
 /**
  * Class CarbonBundle
